eadaf8de18eacbcb90337941985c9350a1790020
Dan <dstein64@users.noreply.github.com>
2016-10-23 21:52:35 -0400

    separate file for examples

---
+++ example.py
@@ -0,0 +1,43 @@
+import pyfm
+
+import numpy as np
+
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
+def mse(actual, predicted):
+    return np.mean((actual - predicted)**2)
+
+def err(actual, predicted):
+    return np.mean(actual != predicted)
+
+# regression
+X, y = datasets.load_boston(return_X_y=True)
+fm = pyfm.FactorizationMachineRegressor(X.shape[1])
+fm.fit(X, y, verbose=False)
+
+f = "fm.model"
+fm.save(f)
+fm2 = pyfm.load(f)
+
+print 'MSE: {}'.format(mse(y, fm.predict(X)))
+print 'MSE: {}'.format(mse(y, fm2.predict(X)))
+
+linear_regression = LinearRegression()
+linear_regression.fit(X, y)
+print 'MSE: {}'.format(mse(y, linear_regression.predict(X)))
+
+# binary classification
+X, y = datasets.load_breast_cancer(return_X_y=True)
+
+fm = pyfm.FactorizationMachineClassifier(X.shape[1])
+fm.fit(X, y, verbose=False)
+print 'Error: {}'.format(err(y, fm.predict(X)))
+
+logistic_regression = LogisticRegression()
+logistic_regression.fit(X, y)
+print 'Error: {}'.format(err(y, logistic_regression.predict(X)))
+
+# multi-class classification
+#X, y = datasets.load_iris(return_X_y=True)
--- pyfm.py
+++ pyfm.py
@@ -2,13 +2,52 @@ import numpy as np
 import theano
 from theano import tensor as T
 
-class _FactorizationMachineBase:
-    floatX = theano.config.floatX
+class FactorizationMachine(object):
+    def __init__(self,
+                 feature_count,
+                 classifier=False,
+                 k = 8,
+                 stdev = 0.1):
+        self.classifier = classifier
+        d = feature_count
 
-    @staticmethod
-    def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
-        grads = T.grad(cost=cost, wrt=params)
+        # *** Symbolic variables ***
+        X = T.matrix()
+        y = T.vector()
+
+        # *** Model parameters ***
+        # bias term
+        w0_init = np.zeros(1)
+        self.w0 = theano.shared(w0_init, allow_downcast=True)
+        # first order coefficients
+        w1_init = np.zeros(d)
+        self.w1 = theano.shared(w1_init, allow_downcast=True)
+        # interaction factors
+        v_init = stdev * np.random.randn(k, d)
+        self.v = theano.shared(v_init, allow_downcast=True)
+
+        # *** The Model ***
+        # the formula for pairwise interactions is from bottom left
+        # of page 997 of Rendle 2010, "Factorization Machines."
+        # This version scales linearly in k and d, as opposed to O(d^2).
+        interactions = 0.5 * T.sum((T.dot(X, T.transpose(self.v)) ** 2) - \
+                                   T.dot(X ** 2, T.transpose(self.v ** 2)), axis=1)
+        y_hat = T.addbroadcast(self.w0,0) + T.dot(X, self.w1) + interactions
+        if self.classifier:
+            y_hat = T.nnet.sigmoid(y_hat)
+
+        # *** Loss Function ***
+        if self.classifier:
+            loss = T.mean(T.nnet.binary_crossentropy(y_hat, y))
+        else:
+            loss = T.mean((y - y_hat)**2)
+
+        # *** Learning ***
         updates = []
+        params = [self.w0, self.w1, self.v]
+        grads = T.grad(cost=loss, wrt=params)
+        # rmsprop
+        lr, rho, epsilon = 0.001, 0.9, 1e-6
         for p, g in zip(params, grads):
             acc = theano.shared(p.get_value() * 0.)
             acc_new = rho * acc + (1 - rho) * g ** 2
@@ -16,74 +55,55 @@ class _FactorizationMachineBase:
             g = g / gradient_scaling
             updates.append((acc, acc_new))
             updates.append((p, p - lr * g))
-        return updates
 
-    def __init__(self):
-        pass
+        self.theano_train = theano.function(
+            inputs=[X, y], outputs=loss, updates=updates, allow_input_downcast=True)
 
-    @staticmethod
-    def _model(X, w0, w1, v):
-        # the formula for pairwise interactions is from bottom left
-        # of page 997 of Rendle 2010, "Factorization Machines."
-        # This version scales linearly in k and d, as opposed to O(d^2).
-        interactions = 0.5 * T.sum((T.dot(X, T.transpose(v)) ** 2) - T.dot(X ** 2, T.transpose(v ** 2)), axis=1)
-        y_hat = T.addbroadcast(w0,0) + T.dot(X, w1) + interactions
+        # *** Prediction ***
+        self.theano_predict = theano.function(
+            inputs=[X], outputs=y_hat, allow_input_downcast=True)
 
-        y_hat = T.nnet.sigmoid(y_hat)
-
-        return y_hat
-
-
-    def fit(self, X, y):
-        """Build a factorization machine from the training set (X, y)."""
-
-        k = 4
-
-        n, d = X.shape
-
-        # *** Symbolic variables ***
-        X_sym = T.matrix()
-        y_sym = T.vector()
-
-        # *** Model parameters ***
-        # bias term
-        self.w0 = theano.shared(np.zeros(1))
-        # first order coefficients
-        self.w1 = theano.shared(np.zeros(d))
-        # interaction factors
-        self.v = theano.shared(np.random.random((k, d))*.01)
-
-        y_hat = _FactorizationMachineBase._model(X_sym, self.w0, self.w1, self.v)
-
-        #loss = T.mean((y_sym - y_hat)**2)
-        loss = T.mean(T.nnet.binary_crossentropy(y_hat, y_sym))
-
-        params = [self.w0, self.w1, self.v]
-        updates = _FactorizationMachineBase.RMSprop(loss, params)
-
-        train = theano.function(
-            inputs=[X_sym, y_sym], outputs=loss, updates=updates, allow_input_downcast=True)
-        self.theano_predict = theano.function(inputs=[X_sym], outputs=y_hat, allow_input_downcast=True)
-
-        epochs = 1000
+    def fit(self, X, y, epochs=100000, verbose=False):
         for i in range(epochs):
-            cost = train(X, y)
-            print i, cost
+            cost = self.theano_train(X, y)
+            if verbose:
+                print i, cost
+
+    def save(self, path):
+        with open(path, 'wb') as f:
+            np.savez(f,
+                     classifier=self.classifier,
+                     w0=self.w0.get_value(),
+                     w1=self.w1.get_value(),
+                     v=self.v.get_value())
+
+class FactorizationMachineClassifier(FactorizationMachine, object):
+    def __init__(self, *args, **kwargs):
+        kwargs['classifier'] = True
+        super(FactorizationMachineClassifier, self).__init__(*args, **kwargs)
+
+    def predict(self, X):
+        return (self.theano_predict(X) > 0.5).astype(np.int)
+
+    def predict_proba(self, X):
+        return self.theano_predict(X)
+
+class FactorizationMachineRegressor(FactorizationMachine, object):
+    def __init__(self, *args, **kwargs):
+        kwargs['classifier'] = False
+        super(FactorizationMachineRegressor, self).__init__(*args, **kwargs)
 
     def predict(self, X):
         return self.theano_predict(X)
 
-    def predict_proba(self, X):
-        pass
-
-from sklearn import datasets
-
-# X, y = datasets.load_iris(return_X_y=True)
-
-X, y = datasets.load_breast_cancer(return_X_y=True)
-
-fm = _FactorizationMachineBase()
-fm.fit(X, y)
-
-print np.mean(y == (fm.predict(X) > 0.5).astype(np.int))
-
+def load(path):
+    meta = np.load(path)
+    classifier = meta['classifier']
+    w0, w1, v = [meta[key] for key in ['w0', 'w1', 'v']]
+    k, d = v.shape
+    cls = FactorizationMachineClassifier if classifier else FactorizationMachineRegressor
+    model = cls(d, k=k)
+    model.w0.set_value(w0)
+    model.w1.set_value(w1)
+    model.v.set_value(v)
+    return model
