3075bc500abcbced6747c0c7ab4e8eb9534f60f9
Dan <dstein64@users.noreply.github.com>
2018-08-17 01:18:39 -0400

    Support Python versions >3.

--- pyfms/utils.py
+++ pyfms/utils.py
@@ -2,11 +2,11 @@ import sys
 
 def xrange(*args, **kwargs):
     major_version = sys.version_info.major
-    if major_version == 3:
-        import builtins
-        return builtins.range(*args, **kwargs)
-    elif major_version == 2:
+    if major_version == 2:
         import __builtin__
         return __builtin__.xrange(*args, **kwargs)
+    elif major_version >= 3:
+        import builtins
+        return builtins.range(*args, **kwargs)
     else:
         raise RuntimeError("Unsupported version of Python.")
