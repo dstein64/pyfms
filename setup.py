bb2fb8c719475b0a526e0356a182e678ee14bb15
Dan <dstein64@users.noreply.github.com>
2020-08-26 20:35:37 -0400

    Use a context manager for reading README.

--- setup.py
+++ setup.py
@@ -6,36 +6,39 @@ version_txt = os.path.join(os.path.dirname(__file__), 'pyfms', 'version.txt')
 with open(version_txt, 'r') as f:
     version = f.read().strip()
 
+with io.open('README.rst', encoding='utf8') as f:
+    long_description = f.read()
+
 setup(
-  author = 'Daniel Steinberg',
-  author_email = 'ds@dannyadam.com',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'Topic :: Scientific/Engineering :: Information Analysis',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: Unix',
-    'Operating System :: POSIX :: Linux',
-    'Operating System :: MacOS',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6'
-  ],
-  description = 'A Theano-based Python implementation of Factorization Machines',
-  extras_require={
-    'dev': ['scikit-learn'],
-  },
-  install_requires = ['numpy', 'theano (>=0.8.0)'],
-  keywords = ['factorization-machines', 'machine-learning'],
-  license = 'MIT',
-  long_description = io.open('README.rst', encoding='utf8').read(),
-  name = 'pyfms',
-  package_data={'pyfms': ['version.txt']},
-  packages=['pyfms'],
-  url = 'https://github.com/dstein64/pyfms',
-  version = version,
+    author='Daniel Steinberg',
+    author_email='ds@dannyadam.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    description='A Theano-based Python implementation of Factorization Machines',
+    extras_require={
+        'dev': ['scikit-learn'],
+    },
+    install_requires=['numpy', 'theano (>=0.8.0)'],
+    keywords=['factorization-machines', 'machine-learning'],
+    license='MIT',
+    long_description=long_description,
+    name='pyfms',
+    package_data={'pyfms': ['version.txt']},
+    packages=['pyfms'],
+    url='https://github.com/dstein64/pyfms',
+    version=version,
 )
